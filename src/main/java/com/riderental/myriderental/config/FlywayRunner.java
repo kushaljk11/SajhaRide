bfe131e5932580c597c8691e4661bc22742dcad6
samikshya karki <karkisamikshya015@gmail.com>
2026-05-01 19:42:10 +0545

    Revert "Refactor database migrations and startup config"
    This reverts commit 085ef72164cbe313d589d751650cb469599493a0.

--- src/main/java/com/riderental/myriderental/config/FlywayRunner.java
+++ src/main/java/com/riderental/myriderental/config/FlywayRunner.java
@@ -2,7 +2,9 @@ package com.riderental.myriderental.config;
 
 import com.riderental.myriderental.util.DBConnection;
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.FlywayException;
 
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,30 +12,26 @@ public class FlywayRunner {
 
     private static final Logger LOGGER = Logger.getLogger(FlywayRunner.class.getName());
 
-    private FlywayRunner() {
-    }
-
-    public static boolean migrate() {
+    public static void migrate() {
         try {
             DBConnection.ensureDatabaseExists();
             DBConnection.validateConnectivity();
+        } catch (SQLException e) {
+            throw new IllegalStateException("Unable to create/access/validate database '" + DBConnection.getDatabaseName() + "' at "
+                    + DBConnection.getJdbcUrl() + ". SQLState=" + e.getSQLState() + ", ErrorCode=" + e.getErrorCode(), e);
+        }
 
-            Flyway flyway = Flyway.configure()
-                    .baselineOnMigrate(true)
-                    .locations("classpath:db/migration")
-                    .dataSource(
-                            DBConnection.getJdbcUrl(),
-                            DBConnection.getUser(),
-                            DBConnection.getPassword()
-                    )
-                    .load();
+        Flyway flyway = Flyway.configure()
+                .baselineOnMigrate(true)
+                .locations("classpath:db/migration")
+                .dataSource(DBConnection.getJdbcUrl(), DBConnection.getUser(), DBConnection.getPassword())
+                .load();
 
+        try {
             flyway.migrate();
-            return true;
-
-        } catch (Exception ex) {
-            LOGGER.log(Level.SEVERE, "Flyway migration failed for DB " + DBConnection.getJdbcUrl(), ex);
-            return false;
+        } catch (FlywayException e) {
+            LOGGER.log(Level.SEVERE, "Flyway migration failed for DB " + DBConnection.getJdbcUrl(), e);
+            throw e;
         }
     }
 }
